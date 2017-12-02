7b569e66dbf0a07df5096bdd7b71f604c6f97e9e
Paul Northrop <p.northrop@ucl.ac.uk>
2017-12-02 17:35:34 +0000

    Add (minimal) main documentation.

---
+++ R/smovie.R
@@ -0,0 +1,11 @@
+#' smovie: some movies to illustrate concepts in statistics
+#'
+#' Add description
+#'
+#' @seealso \code{\link{wws}}: Wald, Wilks and Score test movie.
+#' @seealso \code{\link{shypo}}: Testing simple hypotheses movie.
+#' @docType package
+#' @name smovie
+#' @import methods
+#' @importFrom rpanel rp.control
+NULL
