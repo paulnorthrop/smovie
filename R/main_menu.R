9f1e46364df22abbbd0bc127d28f0f79a7108766
Paul Northrop <p.northrop@ucl.ac.uk>
2018-02-28 19:36:17 +0000

    Hack to enable identical (looking) sub-menu items.

--- R/main_menu.R
+++ R/main_menu.R
@@ -138,7 +138,7 @@ movies <- function(fixed_range = TRUE, hscale = 1) {
     else if (menu.panel$demo == "Maximum") {
       ett(distn = "exponential")
     }
-    else if (menu.panel$demo == "Outliers: leverage and influence") {
+    else if (menu.panel$demo == "Leverage and influence") {
       lev_inf()
     }
     else if (menu.panel$demo == "Wald, Wilks and Score tests") {
@@ -147,43 +147,43 @@ movies <- function(fixed_range = TRUE, hscale = 1) {
     else if (menu.panel$demo == "Testing simple hypotheses") {
       shypo(mu0 = 0, eff = 5, n = 1)
     }
-    else if (menu.panel$demo == "beta") {
+    else if (menu.panel$demo == "beta ") {
       ett(distn = "beta")
     }
-    else if (menu.panel$demo == "Cauchy") {
+    else if (menu.panel$demo == "Cauchy ") {
       ett(distn = "cauchy")
     }
-    else if (menu.panel$demo == "chi-squared") {
+    else if (menu.panel$demo == "chi-squared ") {
       ett(distn = "chi-squared")
     }
-    else if (menu.panel$demo == "exponential") {
+    else if (menu.panel$demo == "exponential ") {
       ett(distn = "exponential")
     }
-    else if (menu.panel$demo == "F") {
+    else if (menu.panel$demo == "F ") {
       ett(distn = "f")
     }
-    else if (menu.panel$demo == "gamma") {
+    else if (menu.panel$demo == "gamma ") {
       ett(distn = "gamma")
     }
-    else if (menu.panel$demo == "generalized Pareto") {
+    else if (menu.panel$demo == "generalized Pareto ") {
       ett(distn = "gp")
     }
-    else if (menu.panel$demo == "log-normal") {
+    else if (menu.panel$demo == "log-normal ") {
       ett(distn = "log-normal")
     }
-    else if (menu.panel$demo == "negated GEV") {
+    else if (menu.panel$demo == "negated GEV ") {
       ett(distn = "ngev")
     }
-    else if (menu.panel$demo == "normal") {
+    else if (menu.panel$demo == "normal ") {
       ett(distn = "normal")
     }
-    else if (menu.panel$demo == "Student t") {
+    else if (menu.panel$demo == "Student t ") {
       ett(distn = "t")
     }
-    else if (menu.panel$demo == "uniform") {
+    else if (menu.panel$demo == "uniform ") {
       ett(distn = "uniform")
     }
-    else if (menu.panel$demo == "Weibull") {
+    else if (menu.panel$demo == "Weibull ") {
       ett(distn = "weibull")
     }
     return(menu.panel)
@@ -218,29 +218,28 @@ movies <- function(fixed_range = TRUE, hscale = 1) {
        "Maximum"
   ),
   list("Regression",
-       "Outliers: leverage and influence"
+       "Leverage and influence"
   ),
   list("Hypothesis testing",
        "Wald, Wilks and Score tests",
        "Testing simple hypotheses"
        ),
   list("Extremal types",
-       "beta",
-       "Cauchy",
-       "chi-squared",
-       "exponential",
-       "F",
-       "gamma",
-       "generalized Pareto",
-       "log-normal",
-       "negated GEV",
-       "normal",
-       "Student t",
-       "uniform",
-       "Weibull"
+       "beta ",
+       "Cauchy ",
+       "chi-squared ",
+       "exponential ",
+       "F ",
+       "gamma ",
+       "generalized Pareto ",
+       "log-normal ",
+       "negated GEV ",
+       "normal ",
+       "Student t ",
+       "uniform ",
+       "Weibull "
        )
   )
-
   demo <- NULL
   rpanel::rp.menu(menu.panel, variable = demo, labels = menu.list,
                   action = panel.launch)
