008ca1a8b6961a4ec442a8f77b9831d5499b6119
Paul Northrop <p.northrop@ucl.ac.uk>
2018-02-28 11:04:45 +0000

    Add general advice.

--- R/main_menu.R
+++ R/main_menu.R
@@ -2,7 +2,8 @@
 #'
 #' Uses the template \code{\link[rpanel]{rp.cartoons}} function to produce
 #' a menu panel from which any of the movies in
-#' \code{\link[smovie]{smovie}} package can be launched.
+#' \code{\link[smovie]{smovie}} package can be launched. For greater control
+#' of an individual example call the relvevant function directly.
 #' @param fixed_range A logical scalar.  Only relevant to the \strong{Discrete}
 #'   and \strong{Continuous} menus.  If \code{TRUE} then in the call to
 #'   \code{\link{discrete}} or \code{\link{continuous}} the argument
